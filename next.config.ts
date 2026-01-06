38fc81ff66bc34e188477ad701f9e07ca32c473d
unknown <ottonowmy@gmail.com>
2026-01-06 22:25:33 +0100

    Supprimer export pour Render

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  // assetPrefix: "/cv/", // pour GitHub Pages dans un sous-dossier
+
 };
 
 module.exports = nextConfig;
